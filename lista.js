2fe049e7593c0cf9afc6ea114df371a93db4d677
Nay <168801233+Eseiza@users.noreply.github.com>
2026-02-11 18:03:34 -0300

    Refactor Firebase initialization and task retrieval

--- lista.js
+++ lista.js
@@ -1,4 +1,27 @@
-// (Misma configuración de Firebase aquí arriba)
+const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJgnFCKt_8TT4BpWrDwqy--Oep0raYA18",
+  authDomain: "romero-env.firebaseapp.com",
+  databaseURL: "https://romero-env-default-rtdb.firebaseio.com",
+  projectId: "romero-env",
+  storageBucket: "romero-env.firebasestorage.app",
+  messagingSenderId: "350498956335",
+  appId: "1:350498956335:web:901f91c4d7b983308252da",
+  measurementId: "G-4WWNKPPBMG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+};
 const db = firebase.database();
 const getFechaHoy = () => new Date().toLocaleDateString('es-AR').replace(/\//g, '-');
 
@@ -27,3 +50,4 @@ window.toggleTarea = (id, estadoActual) => {
         completado: !estadoActual
     });
 };
+
